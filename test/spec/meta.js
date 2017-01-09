001ef97dd7c2b1288b04fd59c2eff6beecd26386
Ricardo Matias <ricardo.matias0@gmail.com>
2017-01-09 15:56:12 +0100

    feat(registry): add a meta information tag
    This allows the user to pass relevant information with the type.
    
    Closes bpmn-io/bpmn-moddle#45

---
+++ test/spec/meta.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var Helper = require('../helper');
+
+
+describe('meta', function() {
+
+  var createModel = Helper.createModelBuilder('test/fixtures/model/');
+  var model = createModel([ 'meta' ]);
+
+  it('should have the "meta" attribute', function() {
+
+    // when
+    var meta = model.getMetaInformation('c:Car');
+
+    // then
+    expect(meta).to.exist;
+    expect(meta).to.be.an('object');
+  });
+
+  it('should have a "owners" property inside "meta"', function() {
+
+    // when
+    var meta = model.getMetaInformation('c:Car');
+
+    // then
+    expect(meta.owners).to.exist;
+    expect(meta.owners).to.eql([ 'the pope', 'donald trump' ]);
+  });
+
+});
