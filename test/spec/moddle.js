36aaaccc0c6ac879ef16199786e24d21aa45a74e
Frank Panno <git@fra-pa.de>
2021-05-20 20:03:04 +0200

    fix: 'localName' and 'name are equal in 'propertiesByName'
    Let 'propertiesByName' point to equal descriptors for property names
    with the same namespace prefix. Take care, that properties which are
    only defined in another ns are also mapped with their 'localName'.

--- test/spec/moddle.js
+++ test/spec/moddle.js
@@ -101,7 +101,10 @@ describe('moddle', function() {
 
       expect(descriptor.ns).to.jsonEqual(expectedDescriptorNs);
       expect(descriptor.properties).to.jsonEqual(expectedDescriptorProperties);
-      expect(descriptor.propertiesByName).to.jsonEqual(expectedDescriptorPropertiesByName);
+      expect(descriptor.propertiesByName.id).to
+        .jsonEqual(expectedDescriptorPropertiesByName.id);
+      expect(descriptor.propertiesByName['props:id']).to
+        .jsonEqual(expectedDescriptorPropertiesByName['props:id']);
     });
 
 
