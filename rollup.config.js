bcbd053f82681b29d7841f1e75b901bcb9bdc4e6
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2022-09-28 10:04:57 +0200

    chore: generate source map

--- rollup.config.js
+++ rollup.config.js
@@ -45,8 +45,8 @@ export default [
   {
     input: srcEntry,
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
+      { file: pkg.main, format: 'cjs', sourcemap: true },
+      { file: pkg.module, format: 'es', sourcemap: true }
     ],
     external: [
       'min-dash'
