a0637bed01957c9a4c3e05785e517c506c58aff7
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2022-10-02 20:14:46 +0200

    chore: update rollup*

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import { terser } from 'rollup-plugin-terser';
 
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 import pkg from './package.json';
 
