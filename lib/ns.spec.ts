b86e4d44ce9f8b4333019f9dcae7243f2f83c6f2
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2025-01-09 15:00:19 +0100

    chore: drop DOM target, this library targets any platform

--- lib/ns.spec.ts
+++ lib/ns.spec.ts
@@ -4,6 +4,5 @@ import { parseName } from './ns.js';
 
 
 const { name, localName, prefix } = parseName('foo:bar');
-console.log('foo:bar', name, localName, prefix);
 
 expectType<{ name: string, localName: string, prefix: string }>(parseName('foo:bar', 'baz'));
