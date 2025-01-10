552c0c6bde56b63fd1c3164eaf5bc2d0ffdf3393
Marty-P <m4rty-p@yandex.com>
2025-01-10 20:50:00 +0700

    polishing types.spec

--- lib/types.spec.ts
+++ lib/types.spec.ts
@@ -11,30 +11,70 @@ import {
   StringType
 } from './types.js';
 
+// when
+const isBuiltInTypes = isBuiltIn('String') && isBuiltIn('Real') && isBuiltIn('Integer') && isBuiltIn('Boolean') &&
+    isBuiltIn('Element');
+// then
+expectType<true>(isBuiltInTypes);
 
+
+// when
+const isBuiltInType = isBuiltIn('foo:bar');
+// then
+expectType<false>(isBuiltInType);
+
+
+// when
+const isSimpleTypes = isSimple('String') && isSimple('Real') && isSimple('Integer') && isSimple('Boolean');
+// then
+expectType<true>(isSimpleTypes);
+
+
+// when
+const isBuiltInPrimitives = isSimple('foo:bar') || isSimple('Element');
+// then
+expectType<false>(isBuiltInPrimitives);
+
+
+// when
+const stringValue = coerceType('String', 'string value');
+// then
+expectType<string>(stringValue);
+
+
+// when
+const realValue = coerceType('Real', '1.34');
+// then
+expectType<number>(realValue);
+
+
+// when
+const intValue = coerceType('Integer', '2');
+// then
+expectType<number>(intValue);
+
+
+// when
+const booleanValue = coerceType('Boolean', '2');
+// then
+expectType<boolean>(booleanValue);
+
+
+// when
+const objValue = coerceType('foo:bar', { foo: 'bar' });
+// then
+expectType<{ foo: string }>(objValue);
+
+
+// when
+const numArrayValue = coerceType('Element', [ 1 ]);
+// then
+expectType<number[]>(numArrayValue);
+
+
+// additional assert valid type values
 expectType<StringType>('String');
 expectType<BooleanType>('Boolean');
 expectType<IntegerType>('Integer');
 expectType<RealType>('Real');
 expectType<ElementType>('Element');
-
-expectType<true>(isBuiltIn('String'));
-expectType<true>(isBuiltIn('Real'));
-expectType<true>(isBuiltIn('Integer'));
-expectType<true>(isBuiltIn('Boolean'));
-expectType<true>(isBuiltIn('Element'));
-expectType<false>(isBuiltIn('foo:bar'));
-
-expectType<true>(isSimple('String'));
-expectType<true>(isSimple('Real'));
-expectType<true>(isSimple('Integer'));
-expectType<true>(isSimple('Boolean'));
-expectType<false>(isSimple('foo:bar'));
-expectType<false>(isSimple('Element'));
-
-expectType<string>(coerceType('String', 'string value'));
-expectType<number>(coerceType('Real', '1'));
-expectType<number>(coerceType('Integer', '2'));
-expectType<boolean>(coerceType('Boolean', '2'));
-expectType<{ foo: 'bar' }>(coerceType('foo:bar', { foo: 'bar' }));
-expectType<number[]>(coerceType('Element', [1]));
