89a4f6d5b534013927e16e3b0f8cc9ac4c51aa95
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2025-12-29 09:34:45 +0100

    chore: bump test dependencies

--- test/expect.js
+++ test/expect.js
@@ -1,9 +1,9 @@
-import chai from 'chai';
+import { use } from 'chai';
 
 import Matchers from './matchers.js';
 
 // add matchers
-chai.use(Matchers);
+use(Matchers);
 
 // expose chai expect
 export {
